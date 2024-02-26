c5f14f3f495f11374b292d7b62c18e1d4477fbc9
Sayantan Chaudhuri <5310@users.noreply.github.com>
2024-02-26 18:59:40 +0000

    Remove incomplete --data-dir test

--- run.sh
+++ run.sh
@@ -17,6 +17,6 @@ podman run -it --rm \
     -e HSA_OVERRIDE_GFX_VERSION="10.3.0" \
     -e venv_dir=../venv \
     -e TORCH_COMMAND="pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7" \
-    -e COMMANDLINE_ARGS="-f --data-dir --listen --enable-insecure-extension-access --no-download-sd-model --precision full --no-half --opt-sub-quad-attention" \
+    -e COMMANDLINE_ARGS="-f --listen --enable-insecure-extension-access --no-download-sd-model --precision full --no-half --opt-sub-quad-attention" \
     rocm-base \
     bash -c "wget -q --no-hsts https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh -O - | bash -s - -f"
