397f0dbe76f4a5fdeb4d0920803a8d0c68da784e
Sayantan Chaudhuri <5310@users.noreply.github.com>
2024-02-28 15:58:40 +0000

    Start of the modular setup script

--- Containerfile
+++ Containerfile
@@ -1,9 +1,14 @@
 FROM docker.io/rocm/dev-ubuntu-22.04:latest
 
 RUN apt-get update &&\
-    apt-get install -y nano wget git python3.10-venv libgoogle-perftools-dev &&\
+    apt-get install -y nano git python3.10-venv libgoogle-perftools-dev &&\
     apt-get clean &&\
     rm -rf /var/lib/apt/lists/*
+
+# RUN curl --location "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz" |\ 
+#     tar -C "/opt" -xz &&\
+#     ln -s /opt/zellij /usr/local/bin/ &&\
+#     echo 'eval "$(zellij setup --generate-auto-start bash)"' >> ~/.bashrc
 
 ENV LD_PRELOAD=libtcmalloc.so
 ENV PIP_NO_CACHE_DIR=true
--- run.sh
+++ run.sh
@@ -1,22 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
-podman build --tag rocm-base - < Containerfile
+ENGINE=docker
+VOLUME=${1:-./volume}
 
-DIR=${1:-./app}
-mkdir -p "$DIR"
-podman run -it --rm \
+$ENGINE build --tag rocm-base - < Containerfile
+
+mkdir -p "$VOLUME"
+$ENGINE run -it --rm \
     --name rocm \
-    --group-add video \
-    --group-add render \
-    --device /dev/kfd:/dev/kfd \
-    --device /dev/dri:/dev/dri \
-    -v "$DIR":/root/app \
-    -w /root/app \
+    `#--group-add video` \
+    `#--group-add render` \
+    `#--device /dev/kfd:/dev/kfd` \
+    `#--device /dev/dri:/dev/dri` \
+    -v "$VOLUME":/root/volume \
+    -w "/root/volume/app" \
+    \
+    `# common` \
+    -e VENV_DIR='/root/volume/environment/venv' \
+    -e REPO_PYTORCH='https://download.pytorch.org/whl/nightly/rocm6.0' \
+    -e PYTORCH_HIP_ALLOC_CONF='garbage_collection_threshold:0.9,max_split_size_mb:256' \
+    -e HSA_OVERRIDE_GFX_VERSION='10.3.0' \
+    \
+    `# sdwebui` \
     -p 7860:7860 \
-    -e PYTORCH_HIP_ALLOC_CONF="garbage_collection_threshold:0.9,max_split_size_mb:256" \
-    -e HSA_OVERRIDE_GFX_VERSION="10.3.0" \
-    -e venv_dir=../venv \
-    -e TORCH_COMMAND="pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7" \
-    -e COMMANDLINE_ARGS="-f --listen --enable-insecure-extension-access --no-download-sd-model --precision full --no-half --opt-sub-quad-attention" \
+    -e ID_SDWEBUI='sdwebui' \
+    -e REPO_SDWEBUI='https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh' \
+    -e LAUNCH_SDWEBUI='
+        curl -sSL -f "$REPO_SDWEBUI" | 
+            clone_dir="$ID_SDWEBUI" 
+            venv_dir="$VENV_DIR" 
+            TORCH_COMMAND="pip install --pre torch torchvision torchaudio --index-url \"$REPO_PYTORCH\""
+            bash -s - -f --listen --enable-insecure-extension-access --data-dir /root/volume/library --no-download-sd-model  --precision full --no-half --opt-sub-quad-attention;
+    ' \
+    `# comfyui` \
+    -p 8188:8188 \
+    -e ID_COMFYUI='comfyui' \
+    -e REPO_COMFYUI='https://github.com/comfyanonymous/ComfyUI' \
+    -e LAUNCH_COMFYUI='
+        if [ ! -e "$ID_COMFYUI" ]; then
+            git clone --depth 1 "$REPO_COMFYUI" "$ID_COMFYUI";
+        fi;
+        cd "$ID_COMFYUI";
+        python3 -m venv "$VENV_DIR";
+        pip install --pre torch torchvision torchaudio --index-url "$REPO_PYTORCH";
+        pip install -r requirements.txt;
+        python3 main.py --listen 0.0.0.0 --disable-auto-launch --dont-upcast-attention `#--force-fp16` `#--fp16-vae` `#--fp32-vae` `#--cpu-vae` `#--lowvram`;
+    ' \
+    \
     rocm-base \
-    bash -c "wget -q --no-hsts https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh -O - | bash -s - -f"
+    bash -c 'eval $LAUNCH_COMFYUI'
