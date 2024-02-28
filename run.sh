b70e5fe947e7cfdea9f3a63a98afa785a219a77a
Sayantan Chaudhuri <5310@users.noreply.github.com>
2024-02-28 16:09:06 +0000

    Slight refactor for clarity

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ENGINE=podman
 VOLUME=${1:-./volume}
+ENGINE=docker
 
 $ENGINE build --tag rocm-base - < Containerfile
 
@@ -17,35 +17,37 @@ $ENGINE run -it --rm \
     \
     `# common` \
     -e VENV_DIR='/root/volume/environment/venv' \
-    -e REPO_PYTORCH='https://download.pytorch.org/whl/nightly/rocm6.0' \
+    -e PYTORCH_REPO='https://download.pytorch.org/whl/nightly/rocm6.0' \
     -e PYTORCH_HIP_ALLOC_CONF='garbage_collection_threshold:0.9,max_split_size_mb:256' \
     -e HSA_OVERRIDE_GFX_VERSION='10.3.0' \
     \
     `# sdwebui` \
     -p 7860:7860 \
-    -e ID_SDWEBUI='sdwebui' \
-    -e REPO_SDWEBUI='https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh' \
-    -e LAUNCH_SDWEBUI='
-        curl -sSL -f "$REPO_SDWEBUI" | 
-            clone_dir="$ID_SDWEBUI" 
+    -e SDWEBUI_NAME='sdwebui' \
+    -e SDWEBUI_REPO='https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh' \
+    -e SDWEBUI_ARGS='-f --listen --enable-insecure-extension-access --data-dir /root/volume/library --no-download-sd-model  --precision full --no-half --opt-sub-quad-attention' \
+    -e SDWEBUI_RUN='
+        curl -sSL -f "$SDWEBUI_REPO" | 
+            clone_dir="$SDWEBUI_NAME" 
             venv_dir="$VENV_DIR" 
-            TORCH_COMMAND="pip install --pre torch torchvision torchaudio --index-url \"$REPO_PYTORCH\""
-            bash -s - -f --listen --enable-insecure-extension-access --data-dir /root/volume/library --no-download-sd-model  --precision full --no-half --opt-sub-quad-attention;
+            TORCH_COMMAND="pip install --pre torch torchvision torchaudio --index-url \"$PYTORCH_REPO\""
+            bash -s - $SDWEBUI_ARGS;
     ' \
     `# comfyui` \
     -p 8188:8188 \
-    -e ID_COMFYUI='comfyui' \
-    -e REPO_COMFYUI='https://github.com/comfyanonymous/ComfyUI' \
-    -e LAUNCH_COMFYUI='
-        if [ ! -e "$ID_COMFYUI" ]; then
-            git clone --depth 1 "$REPO_COMFYUI" "$ID_COMFYUI";
+    -e COMFYUI_NAME='comfyui' \
+    -e COMFYUI_REPO='https://github.com/comfyanonymous/ComfyUI' \
+    -e COMFYUI_ARGS='--listen 0.0.0.0 --disable-auto-launch --dont-upcast-attention `#--force-fp16` `#--fp16-vae` `#--fp32-vae` `#--cpu-vae` `#--lowvram`' \
+    -e COMFYUI_RUN='
+        if [ ! -e "$COMFYUI_NAME" ]; then
+            git clone --depth 1 "$COMFYUI_REPO" "$COMFYUI_NAME";
         fi;
-        cd "$ID_COMFYUI";
+        cd "$COMFYUI_NAME";
         python3 -m venv "$VENV_DIR";
-        pip install --pre torch torchvision torchaudio --index-url "$REPO_PYTORCH";
+        pip install --pre torch torchvision torchaudio --index-url "$PYTORCH_REPO";
         pip install -r requirements.txt;
-        python3 main.py --listen 0.0.0.0 --disable-auto-launch --dont-upcast-attention `#--force-fp16` `#--fp16-vae` `#--fp32-vae` `#--cpu-vae` `#--lowvram`;
+        python3 main.py $COMFYUI_ARGS;
     ' \
     \
     rocm-base \
-    bash -c 'eval $LAUNCH_COMFYUI'
+    bash -c 'eval $COMFYUI_RUN'
