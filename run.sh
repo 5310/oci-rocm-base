332d888d7b3f02fd5415be9d29559c6adb84d3f9
Sayantan Chaudhuri <5310@users.noreply.github.com>
2024-03-02 14:38:58 +0000

    Lint

--- run.sh
+++ run.sh
@@ -89,7 +89,7 @@ case $APP in
 			EOF
 			git clone --depth 1 "$COMFYUI_REPO" "$COMFYUI_NAME"
 			if [ ! -e "custom_nodes/ComfyUI-Manager" ]; then
-				git clone --depth 1 "$COMFYUI_MANAGER_REPO" custom_nodes/ComfyUI-Manager
+				git clone --depth 1 "$COMFYUI_MANAGER_REPO" "custom_nodes/ComfyUI-Manager"
 			fi
 			python3 -m venv "$VENV_DIR"
 			source "$VENV_DIR/bin/activate"
